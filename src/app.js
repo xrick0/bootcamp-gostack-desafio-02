262e819496832f6f756d7c5f630560b988c5fa70
Ricardo Marcon <ricardo.f.marcon@gmail.com>
2020-04-07 15:18:46 -0300

    Challenge completed

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const { uuid, isUuid } = require('uuidv4')
 const express = require("express");
 const cors = require("cors");
 
@@ -10,24 +11,108 @@ app.use(cors());
 
 const repositories = [];
 
+function idIsValid(request, response, next) {
+  const { id } = request.params
+
+  if (!isUuid(id)) return response.status(400).json({error: 'Invalid ID!'})
+
+  return next()
+}
+
+function idIsRegistered(request, response, next) {
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => 
+    (repository.id === id)
+  )
+
+  if (repositoryIndex < 0) return response.status(400).json({
+    error: 'No repository was found with the given ID!'
+  })
+
+  return next()
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  if (!title || !url || !techs) return response.send(400)
+
+  const id = uuid()
+
+  const newRepository ={
+    id,
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(newRepository)
+
+  return response.json(newRepository)
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", idIsValid, idIsRegistered, (request, response) => {
+  const { id } = request.params
+  let { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(repository => 
+    (repository.id === id)
+  )
+
+  if (!title) title = repositories[repositoryIndex].title
+  if (!url) url = repositories[repositoryIndex].url
+  if (!techs) techs = repositories[repositoryIndex].techs
+
+  const modifiedRepository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+
+  repositories[repositoryIndex] = modifiedRepository
+
+  return response.json(modifiedRepository)
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
-});
+app.delete("/repositories/:id", 
+  idIsValid, 
+  idIsRegistered, 
+  (request, response) => {
+    const { id } = request.params
+    let { title, url, techs } = request.body
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+    const repositoryIndex = repositories.findIndex(repository => 
+      (repository.id === id)
+    )
+
+    repositories.splice(repositoryIndex, 1)
+
+    return response.status(204).send()
+  }
+);
+
+app.post("/repositories/:id/like", 
+  idIsValid, 
+  idIsRegistered, 
+  (request, response) => {
+    const { id } = request.params
+
+    const repositoryIndex = repositories.findIndex(repository => 
+      (repository.id === id)
+    )
+
+    repositories[repositoryIndex].likes += 1
+
+    return response.json({ likes: repositories[repositoryIndex].likes })
+  }
+);
 
 module.exports = app;
